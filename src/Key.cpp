b7dcca643be5ad8b2518136bb93f8c92dabcdc61
绎凡 金 <18107907861@163.com>
2026-01-25 19:39:07 +0800

    add structure `KeyDownListener` to `Key.hpp&cpp`

--- src/Key.cpp
+++ src/Key.cpp
@@ -5,8 +5,28 @@
 #include "SokuAddresses.hpp"
 #include "Key.hpp"
 
+#include "UnionCast.hpp"
+
 namespace SokuLib
 {
-	// �V�X�e���L�[�����V���b�g
-	bool (* const checkKeyOneshot)(int, int, int, int) = reinterpret_cast<bool (*)(int, int, int, int)>(ADDR_CHECK_KEY_ONESHOT);
+	//g_keyDownListener.checkPress()
+	bool (* const checkKeyOneshot)(unsigned char, bool, bool, bool) = reinterpret_cast<bool (*)(unsigned char, bool, bool, bool)>(ADDR_CHECK_KEY_ONESHOT);
+
+	KeyDownListener& g_keyDownListener = *reinterpret_cast<KeyDownListener*>(0x8998d8);
+
+	void KeyDownListener::clear() {
+		return (this->*union_cast<void(KeyDownListener::*)()>(0x40a910))();
+	}
+	void KeyDownListener::refresh() {
+		return (this->*union_cast<void(KeyDownListener::*)()>(0x40a950))();
+	}
+	bool KeyDownListener::checkPress(unsigned char keyCode, bool shift, bool alt, bool ctrl) {
+		return (this->*union_cast<bool(KeyDownListener::*)(unsigned char, bool, bool, bool)>(0x40aa60))(keyCode, shift, alt, ctrl);
+	}
+	void KeyDownListener::setCounter(unsigned char keyCode, unsigned char value) {
+		return (this->*union_cast<void(KeyDownListener::*)(unsigned char, unsigned char)>(0x40aab0))(keyCode, value);
+	}
+	void KeyDownListener::enableListen(unsigned char keyCode) {
+		return (this->*union_cast<void(KeyDownListener::*)(unsigned char)>(0x40aac0))(keyCode);
+	}
 }
--- src/Key.hpp
+++ src/Key.hpp
@@ -5,10 +5,29 @@
 #ifndef SOKULIB_KEY_HPP
 #define SOKULIB_KEY_HPP
 
+#include "Vector.hpp"
 
 namespace SokuLib
 {
-	extern bool (* const checkKeyOneshot)(int, int, int, int);
+	struct KeyDownListener {
+		int count[256]; // counter for DIK keydown
+		Vector<unsigned char> enabledKeys; // enlisted keydown
+		bool shift;// true if L/R shift is keydown
+		bool alt;
+		bool ctrl;
+
+		void clear();
+		void refresh();
+		bool checkPress(unsigned char keyCode, bool shift, bool alt, bool ctrl);
+		void setCounter(unsigned char keyCode, unsigned char value);
+		
+		void enableListen(unsigned char keyCode);
+		
+	};
+
+	extern KeyDownListener &g_keyDownListener;
+
+	extern bool (* const checkKeyOneshot)(unsigned char, bool, bool, bool);
 }
 
 
